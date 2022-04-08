c3697e02c7fbcf50c1602de63e15d05aed5c717f
Dillii <43846532+Dillii@users.noreply.github.com>
2022-04-08 03:36:56 +0300

    Add files via upload

---
+++ XML_to_DB_Loader/Interfaces/IDataTypeParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace XML_To_DB_Loader.Interfaces
+{
+    /// <summary>
+    /// Implements methods to parse file name into model types
+    /// 
+    /// In order not to engage in manual file to model Type conversion
+    /// you need to find relationship between file name and Type name of your models
+    /// </summary>
+    public interface IDataTypeParser
+    {
+        /// <summary>
+        /// File to model type Parser
+        /// </summary>
+        /// <param name="file">file to parse</param>
+        /// <returns>Model Type</returns>
+        Type PrseXmlFileNameToModelType(FileInfo file);
+    }
+}
